c557b039990c9568e6cc9648716162dab1a3f92a
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-04-30 18:32:47 +0200

    Cleanup Database and add artist model

---
+++ database/src/main/java/de/schnettler/database/models/Artist.kt
@@ -0,0 +1,10 @@
+package de.schnettler.database.models
+
+data class Artist(
+    val name: String,
+    val playcount: String,
+    val listeners: String,
+    val mbid: String,
+    val url: String,
+    val streamable: String
+)
