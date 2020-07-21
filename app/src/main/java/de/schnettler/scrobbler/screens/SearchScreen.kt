ffbe6a3a4ea3923f1fd1d6f6b6ac98c3dcda272e
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2020-07-21 19:26:20 +0200

    SearchScreen: Improve search results

--- app/src/main/java/de/schnettler/scrobbler/screens/SearchScreen.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/SearchScreen.kt
@@ -5,19 +5,25 @@ import androidx.compose.collectAsState
 import androidx.compose.state
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Box
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
-import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.lazy.LazyColumnItems
 import androidx.ui.input.ImeAction
 import androidx.ui.input.TextFieldValue
 import androidx.ui.layout.Column
 import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.padding
+import androidx.ui.material.Divider
 import androidx.ui.material.FilledTextField
+import androidx.ui.material.ListItem
+import androidx.ui.res.colorResource
+import androidx.ui.res.vectorResource
 import androidx.ui.unit.dp
 import com.dropbox.android.external.store4.StoreResponse
 import de.schnettler.database.models.Artist
 import de.schnettler.database.models.CommonEntity
-import de.schnettler.scrobbler.components.GenericAdapterList
+import de.schnettler.scrobbler.R
+import de.schnettler.scrobbler.components.PlainListIconBackground
 import de.schnettler.scrobbler.viewmodels.SearchViewModel
 
 @Composable
@@ -41,9 +47,17 @@ fun SearchScreen(model: SearchViewModel, onItemSelected: (CommonEntity) -> Unit)
             )
         }
         if (result.value is StoreResponse.Data) {
-            GenericAdapterList(data = (result.value as StoreResponse.Data<List<Artist>>).value, onListingSelected = {
-              onItemSelected(it)
-            })
+            LazyColumnItems(items = (result.value as StoreResponse.Data<List<Artist>>).value) {
+                ListItem(
+                    text = { Text(it.name) },
+                    secondaryText = { Text("${formatter.format(it.listeners)} Listeners") },
+                    icon = { PlainListIconBackground {
+                        Icon(vectorResource(id = R.drawable.ic_outline_account_circle_24))
+                    } },
+                    onClick = { onItemSelected(it) }
+                )
+                Divider(color = colorResource(id = R.color.colorStroke), startIndent = 72.dp)
+            }
         }
     }
 }
