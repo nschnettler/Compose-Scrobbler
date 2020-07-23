350e4bf69888fe28da5062bc1fa8480d077be43d
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2020-07-23 17:05:01 +0200

    Compose dev15 (3/3): Rewrite Swipe2Refresh based on new stateDraggable Modifier

--- app/src/main/java/de/schnettler/scrobbler/components/SwipeToRefresh.kt
+++ app/src/main/java/de/schnettler/scrobbler/components/SwipeToRefresh.kt
@@ -1,19 +1,18 @@
 package de.schnettler.scrobbler.components
 
 import androidx.animation.*
+import androidx.annotation.FloatRange
 import androidx.compose.Composable
 import androidx.compose.onCommit
 import androidx.compose.remember
 import androidx.compose.state
-import androidx.ui.animation.animatedFloat
-import androidx.ui.core.Alignment
-import androidx.ui.core.DensityAmbient
-import androidx.ui.core.Modifier
-import androidx.ui.core.PassThroughLayout
+import androidx.ui.animation.asDisposableClock
+import androidx.ui.core.*
+import androidx.ui.core.gesture.scrollorientationlocking.Orientation
 import androidx.ui.foundation.Box
-import androidx.ui.foundation.animation.AnchorsFlingConfig
+import androidx.ui.foundation.InteractionState
+import androidx.ui.foundation.animation.FlingConfig
 import androidx.ui.foundation.animation.fling
-import androidx.ui.foundation.gestures.DragDirection
 import androidx.ui.foundation.gestures.draggable
 import androidx.ui.foundation.shape.corner.CircleShape
 import androidx.ui.layout.Stack
@@ -23,6 +22,8 @@ import androidx.ui.layout.preferredSize
 import androidx.ui.material.CircularProgressIndicator
 import androidx.ui.material.Surface
 import androidx.ui.unit.dp
+import androidx.ui.util.fastFirstOrNull
+import kotlin.math.sign
 
 private val SWIPE_DISTANCE_SIZE = 100.dp
 private const val SWIPE_DOWN_OFFSET = 1.2f
@@ -38,15 +39,19 @@ fun SwipeToRefreshLayout(
     // min is below negative to hide
     val min = -size
     val max = size * SWIPE_DOWN_OFFSET
-    StateDraggable(
+    val dragPosition = state { 0f }
+    Box(Modifier.stateDraggable(
         state = refreshingState,
         onStateChange = { shouldRefresh -> if (shouldRefresh) onRefresh() },
         anchorsToState = listOf(min to false, max to true),
-        animationBuilder = TweenBuilder(),
-        dragDirection = DragDirection.Vertical,
         minValue = min,
-        maxValue = max
-    ) { dragPosition ->
+        maxValue = max,
+        animationSpec = TweenSpec(),
+        orientation = Orientation.Vertical,
+        onNewValue = { newValue ->
+            dragPosition.value = newValue
+        }
+    )) {
         val dpOffset = with(DensityAmbient.current) {
             (dragPosition.value * 0.5f).toDp()
         }
@@ -98,59 +103,114 @@ fun SwipeRefreshPrograssIndicator() {
  * @param enabled whether or not this Draggable is enabled and should consume events
  */
 // TODO(malkov/tianliu) (figure our how to make it better and make public)
-@Composable
-internal fun <T> StateDraggable(
+internal fun <T> Modifier.stateDraggable(
     state: T,
     onStateChange: (T) -> Unit,
     anchorsToState: List<Pair<Float, T>>,
-    animationBuilder: AnimationBuilder<Float>,
-    dragDirection: DragDirection,
+    animationSpec: AnimationSpec<Float>,
+    orientation: Orientation,
+    thresholds: (Float, Float) -> Float = fractionalThresholds(0.5f),
     enabled: Boolean = true,
+    reverseDirection: Boolean = false,
     minValue: Float = Float.MIN_VALUE,
     maxValue: Float = Float.MAX_VALUE,
-    content: @Composable() (AnimatedFloat) -> Unit
-) {
+    interactionState: InteractionState? = null,
+    onNewValue: (Float) -> Unit
+) = composed {
     val forceAnimationCheck = state { true }
-
     val anchors = remember(anchorsToState) { anchorsToState.map { it.first } }
-    val currentValue = anchorsToState.firstOrNull { it.second == state }!!.first
-    val flingConfig =
-        AnchorsFlingConfig(
-            anchors = anchors,
-            animationBuilder = animationBuilder,
-            onAnimationEnd = { reason, finalValue, _ ->
-                if (reason != AnimationEndReason.Interrupted) {
-                    val newState = anchorsToState.firstOrNull { it.first == finalValue }?.second
-                    if (newState != null && newState != state) {
-                        onStateChange(newState)
-                        forceAnimationCheck.value = !forceAnimationCheck.value
-                    }
-                }
+    val currentValue = anchorsToState.fastFirstOrNull { it.second == state }!!.first
+    val onAnimationEnd: OnAnimationEnd = { reason, finalValue, _ ->
+        if (reason != AnimationEndReason.Interrupted) {
+            val newState = anchorsToState.firstOrNull { it.first == finalValue }?.second
+            if (newState != null && newState != state) {
+                onStateChange(newState)
+                forceAnimationCheck.value = !forceAnimationCheck.value
             }
-        )
-    val position = animatedFloat(currentValue)
+        }
+    }
+    val flingConfig = FlingConfig(
+        decayAnimation = ExponentialDecay(),
+        adjustTarget = { target ->
+            // Find the two anchors the target lies between.
+            val a = anchors.filter { it <= target }.maxOrNull()
+            val b = anchors.filter { it >= target }.minOrNull()
+            // Compute which anchor to fling to.
+            val adjusted: Float =
+                if (a == null && b == null) {
+                    // There are no anchors, so return the target unchanged.
+                    target
+                } else if (a == null) {
+                    // The target lies below the anchors, so return the first anchor (b).
+                    b!!
+                } else if (b == null) {
+                    // The target lies above the anchors, so return the last anchor (b).
+                    a
+                } else if (a == b) {
+                    // The target is equal to one of the anchors, so return the target unchanged.
+                    target
+                } else {
+                    // The target lies strictly between the two anchors a and b.
+                    // Compute the threshold between a and b based on the drag direction.
+                    val threshold = if (currentValue <= a) {
+                        thresholds(a, b)
+                    } else {
+                        thresholds(b, a)
+                    }
+                    require(threshold >= a && threshold <= b) {
+                        "Invalid threshold $threshold between anchors $a and $b."
+                    }
+                    if (target < threshold) a else b
+                }
+            TargetAnimation(adjusted, animationSpec)
+        }
+    )
+    val clock = AnimationClockAmbient.current.asDisposableClock()
+    val position = remember(clock) {
+        onNewValue(currentValue)
+        NotificationBasedAnimatedFloat(currentValue, clock, onNewValue)
+    }
+    position.onNewValue = onNewValue
     position.setBounds(minValue, maxValue)
-
     // This state is to force this component to be recomposed and trigger onCommit below
     // This is needed to stay in sync with drag state that caller side holds
     onCommit(currentValue, forceAnimationCheck.value) {
-        position.animateTo(currentValue, animationBuilder)
+        position.animateTo(currentValue, animationSpec)
     }
-    val draggable = Modifier.draggable(
-        dragDirection = dragDirection,
-        onDragDeltaConsumptionRequested = { delta ->
-            val old = position.value
-            position.snapTo(position.value + delta)
-            position.value - old
-        },
-        onDragStopped = { position.fling(flingConfig, it) },
+    Modifier.draggable(
+        orientation = orientation,
         enabled = enabled,
-        startDragImmediately = position.isRunning
-    )
-    // TODO(b/150706555): This layout is temporary and should be removed once Semantics
-    //  is implemented with modifiers.
-    @Suppress("DEPRECATION")
-    PassThroughLayout(draggable) {
-        content(position)
+        reverseDirection = reverseDirection,
+        startDragImmediately = position.isRunning,
+        interactionState = interactionState,
+        onDragStopped = { position.fling(it, flingConfig, onAnimationEnd) }
+    ) { delta ->
+        position.snapTo(position.value + delta)
     }
 }
+
+/**
+ * Fixed anchors thresholds. Each threshold will be at an [offset] away from the first anchor.
+ */
+internal fun fixedThresholds(offset: Float): (Float, Float) -> Float =
+    { fromAnchor, toAnchor -> fromAnchor + offset * sign(toAnchor - fromAnchor) }
+/**
+ * Fractional thresholds. Each threshold will be at a [fraction] of the way between the two anchors.
+ */
+internal fun fractionalThresholds(
+    @FloatRange(from = 0.0, to = 1.0) fraction: Float
+): (Float, Float) -> Float = { fromAnchor, toAnchor -> lerp(fromAnchor, toAnchor, fraction) }
+private class NotificationBasedAnimatedFloat(
+    initial: Float,
+    clock: AnimationClockObservable,
+    internal var onNewValue: (Float) -> Unit
+) : AnimatedFloat(clock, Spring.DefaultDisplacementThreshold) {
+    override var value = initial
+        set(value) {
+            onNewValue(value)
+            field = value
+        }
+}
+
+internal fun lerp(start: Float, stop: Float, fraction: Float) =
+    (start * (1 - fraction) + stop * fraction)
