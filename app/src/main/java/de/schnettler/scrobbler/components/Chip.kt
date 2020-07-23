87f0472d1c79fdc79efb7c35d469790f1b24bdc8
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2020-07-23 12:13:39 +0200

    Improve Chips (Selected State, Improved Colors)

--- app/src/main/java/de/schnettler/scrobbler/components/Chip.kt
+++ app/src/main/java/de/schnettler/scrobbler/components/Chip.kt
@@ -1,21 +1,22 @@
 package de.schnettler.scrobbler.components
 
 import androidx.compose.Composable
+import androidx.compose.getValue
+import androidx.compose.setValue
+import androidx.compose.state
+import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Box
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.clickable
-import androidx.ui.foundation.drawBackground
+import androidx.ui.foundation.drawBorder
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
-import androidx.ui.layout.ExperimentalLayout
-import androidx.ui.layout.FlowRow
-import androidx.ui.layout.fillMaxHeight
-import androidx.ui.layout.padding
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
-import androidx.ui.res.colorResource
+import androidx.ui.material.Surface
 import androidx.ui.text.style.TextOverflow
 import androidx.ui.unit.dp
-import de.schnettler.scrobbler.R
 
 @OptIn(ExperimentalLayout::class)
 @Composable
@@ -30,16 +31,26 @@ fun ChipRow(items: List<String>, onChipClicked: (String) -> Unit = {}) {
 }
 
 @Composable
-fun Chip(text: String, onSelected: () -> Unit) {
-    Box(modifier = Modifier.clickable(onClick = { onSelected() })) {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.body2,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier.drawBackground(
-                color = colorResource(id = R.color.colorBackgroundElevated),
-                shape = RoundedCornerShape(25.dp)
-            ) + Modifier.padding(horizontal = 12.dp, vertical = 8.dp) + Modifier.fillMaxHeight()
-        )
+fun Chip(
+    text: String,
+    selected: Boolean = false,
+    colorSelected: Color = MaterialTheme.colors.secondary.copy(alpha = 0.4f),
+    colorNormal: Color = MaterialTheme.colors.onSurface.copy(alpha = 0.12f),
+    onSelected: () -> Unit) {
+
+    Surface(
+        shape = RoundedCornerShape(25.dp),
+        color = if (selected) colorSelected else colorNormal
+    ) {
+        Box(gravity = Alignment.Center,
+            modifier = Modifier.clickable(onClick = { onSelected() }).preferredHeight(32.dp)
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.body2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
     }
 }
