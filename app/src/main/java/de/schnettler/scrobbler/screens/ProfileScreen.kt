b698fc8013ce505909dab0966a324b0ede84ef32
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-07-28 14:02:10 +0200

    ProfileScreen.kt: Replace deprecated RadioGroupTextItem

--- app/src/main/java/de/schnettler/scrobbler/screens/ProfileScreen.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/ProfileScreen.kt
@@ -11,9 +11,11 @@ import androidx.ui.core.tag
 import androidx.ui.foundation.Box
 import androidx.ui.foundation.ScrollableColumn
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.selection.selectable
 import androidx.ui.foundation.shape.corner.CircleShape
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.Column
+import androidx.ui.layout.Row
 import androidx.ui.layout.fillMaxSize
 import androidx.ui.layout.fillMaxWidth
 import androidx.ui.layout.padding
@@ -23,6 +25,7 @@ import androidx.ui.material.AlertDialog
 import androidx.ui.material.Card
 import androidx.ui.material.ListItem
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.RadioButton
 import androidx.ui.material.RadioGroup
 import androidx.ui.material.Surface
 import androidx.ui.material.TextButton
@@ -158,9 +161,28 @@ private fun PeriodSelectDialog(
         text = {
             RadioGroup {
                 radioGroupOptions.forEach {
-                    RadioGroupTextItem(selected = selected == it, onSelect = {
+                    val isSelected = selected == it
+                    val onSelected = {
                         selected = it
-                    }, text = it.niceName)
+                    }
+                    Box(
+                        modifier = Modifier.selectable(
+                            selected = isSelected,
+                            onClick = { if (!isSelected) onSelected() }
+                        ),
+                        children = {
+                            Box {
+                                Row(Modifier.fillMaxWidth().padding(16.dp)) {
+                                    RadioButton(selected = isSelected, onClick = onSelected)
+                                    Text(
+                                        text = it.niceName,
+                                        style = MaterialTheme.typography.body1.merge(),
+                                        modifier = Modifier.padding(start = 16.dp)
+                                    )
+                                }
+                            }
+                        }
+                    )
                 }
             }
         },
