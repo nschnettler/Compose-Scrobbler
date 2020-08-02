9829f60ec23360d1ec845d1f2471c95eeee9e5b7
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-02 13:54:54 +0200

    ArtistDetails.kt: Improve Backdrop (Placeholder, Loading Indicator, Background color)

--- app/src/main/java/de/schnettler/scrobbler/screens/details/ArtistDetails.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/details/ArtistDetails.kt
@@ -1,21 +1,32 @@
 package de.schnettler.scrobbler.screens.details
 
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
 import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
+import androidx.ui.foundation.Box
+import androidx.ui.foundation.ContentGravity
 import androidx.ui.foundation.ScrollableColumn
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.drawBackground
+import androidx.ui.layout.RowScope.gravity
 import androidx.ui.layout.Stack
 import androidx.ui.layout.aspectRatio
 import androidx.ui.layout.fillMaxSize
+import androidx.ui.layout.wrapContentWidth
+import androidx.ui.material.CircularProgressIndicator
 import androidx.ui.material.ListItem
+import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
+import androidx.ui.res.colorResource
 import androidx.ui.unit.dp
 import de.schnettler.database.models.EntityWithStats.TrackWithStats
 import de.schnettler.database.models.EntityWithStatsAndInfo.ArtistWithStatsAndInfo
 import de.schnettler.database.models.LastFmEntity
+import de.schnettler.scrobbler.R
 import de.schnettler.scrobbler.components.ListeningStats
 import de.schnettler.scrobbler.components.ListingScroller
+import de.schnettler.scrobbler.components.LiveDataLoadingComponent
 import de.schnettler.scrobbler.components.PlainListIconBackground
 import de.schnettler.scrobbler.components.TitleComponent
 import de.schnettler.scrobbler.util.PlaysStyle
@@ -30,7 +41,7 @@ fun ArtistDetailScreen(
 ) {
     val (artist, stats, info) = artistInfo
     ScrollableColumn {
-        Backdrop(imageUrl = artist.imageUrl, modifier = Modifier.aspectRatio(16 / 10f))
+        Backdrop(imageUrl = artist.imageUrl, modifier = Modifier.aspectRatio(16 / 10f), placeholder = artist.name)
         AlbumDescription(description = info.wiki)
         ListeningStats(item = stats)
         TagCategory(tags = info.tags, onTagClicked = onTagClicked)
@@ -72,17 +83,19 @@ fun TrackListWithStats(tracks: List<TrackWithStats>, onListingSelected: (LastFmE
 @Composable
 private fun Backdrop(
     imageUrl: String?,
+    placeholder: String,
     modifier: Modifier
 ) {
-    Surface(modifier = modifier) {
-        Stack(Modifier.fillMaxSize()) {
-            imageUrl?.let {
-                CoilImageWithCrossfade(
-                    data = it,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-        }
+    Box(modifier = modifier.fillMaxSize().drawBackground(MaterialTheme.colors.onSurface.copy(0.05F)), gravity = ContentGravity.Center) {
+        imageUrl?.let {
+            CoilImageWithCrossfade(
+                data = it,
+                contentScale = ContentScale.Crop,
+                loading = {
+                    LiveDataLoadingComponent()
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+        } ?: Text(text = placeholder, style = MaterialTheme.typography.h3)
     }
 }
