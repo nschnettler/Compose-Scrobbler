966a018556b86ca2f0dff8a97257af7d108f317d
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-15 13:08:03 +0200

    Add more Database Tests (AuthDao, ChartDao, SessionDao)

--- database/src/androidTest/java/de/schnettler/database/DataGenerator.kt
+++ database/src/androidTest/java/de/schnettler/database/DataGenerator.kt
@@ -1,10 +1,16 @@
 package de.schnettler.database
 
 import de.schnettler.database.models.EntityInfo
+import de.schnettler.database.models.EntityType
 import de.schnettler.database.models.EntityWithStats
 import de.schnettler.database.models.EntityWithStatsAndInfo
 import de.schnettler.database.models.LastFmEntity
+import de.schnettler.database.models.ListType
 import de.schnettler.database.models.Stats
+import de.schnettler.database.models.TopListAlbum
+import de.schnettler.database.models.TopListArtist
+import de.schnettler.database.models.TopListEntry
+import de.schnettler.database.models.TopListTrack
 import kotlin.random.Random
 
 object DataGenerator {
@@ -41,6 +47,30 @@ object DataGenerator {
         )
     }
 
+    fun generateArtistWithTopListEntry(count: Int, listType: ListType) = List(count) {
+        val artist = generateArtist(it)
+        TopListArtist(
+            TopListEntry(artist.id, EntityType.ARTIST, listType, it, 0),
+            artist
+        )
+    }
+
+    fun generateTracksWithTopListEntry(count: Int, listType: ListType) = List(count) {
+        val track = generateTrack(it)
+        TopListTrack(
+            TopListEntry(id = track.id, entityType = EntityType.TRACK, listType = listType, index = it, count = 0),
+            track
+        )
+    }
+
+    fun generateAlbumWithTopListEntry(count: Int, listType: ListType, artist: String = "artist") = List(count) {
+        val album = generateAlbum(it, artist)
+        TopListAlbum(
+            TopListEntry(id = album.id, entityType = EntityType.ALBUM, listType = listType, index = it, count = 0),
+            album
+        )
+    }
+
     private fun generateStats(forId: String) = Stats(forId, Random.nextLong(10))
 
     private fun generateInfo(forId: String) = EntityInfo(forId, emptyList(), 10, "wiki")
@@ -49,6 +79,13 @@ object DataGenerator {
         generateArtist(it, prefix)
     }
 
+    fun generateTracks(number: Int, prefix: String = "track", artist: String = "artist") = List(number) {
+        generateTrack(it, prefix, artist)
+    }
+
     private fun generateArtist(current: Int = 0, prefix: String = "artist") =
         LastFmEntity.Artist("$prefix$current", "url$current")
+
+    private fun generateTrack(current: Int = 0, prefix: String = "track", artist: String = "artist") =
+        LastFmEntity.Track("$prefix$current", "url$current", artist)
 }
