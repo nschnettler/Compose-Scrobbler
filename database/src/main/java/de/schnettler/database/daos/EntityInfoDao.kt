8cec408a247c026700a5611c12b0188d973bcc91
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-18 14:18:29 +0200

    Database: Add tests for database migrations

--- database/src/main/java/de/schnettler/database/daos/EntityInfoDao.kt
+++ database/src/main/java/de/schnettler/database/daos/EntityInfoDao.kt
@@ -1,7 +1,11 @@
 package de.schnettler.database.daos
 
 import androidx.room.Dao
+import androidx.room.Query
 import de.schnettler.database.models.EntityInfo
 
 @Dao
-abstract class EntityInfoDao : BaseDao<EntityInfo>
+abstract class EntityInfoDao : BaseDao<EntityInfo> {
+    @Query("SELECT * FROM entity_info WHERE id = :id")
+    abstract fun get(id: String): EntityInfo
+}
