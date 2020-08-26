53f891f70e5271f28caaa9cbaad78d63f94f423b
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-08-26 19:30:57 +0200

    Repo: Add missing test dependencies

--- repo/build.gradle.kts
+++ repo/build.gradle.kts
@@ -10,11 +10,18 @@ android {
 
     defaultConfig {
         minSdkVersion(21)
+        targetSdkVersion(30)
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    packagingOptions {
+        excludes += "/META-INF/AL2.0"
+        excludes += "/META-INF/LGPL2.1"
     }
 }
 
@@ -33,4 +40,13 @@ dependencies {
 
     api("com.dropbox.mobile.store", "store4", "_")
     api("com.github.tfcporciuncula", "flow-preferences", "_")
+
+    androidTestImplementation(Testing.junit4)
+    androidTestImplementation(KotlinX.coroutines.test)
+    androidTestImplementation(AndroidX.test.ext.junitKtx)
+    androidTestImplementation(AndroidX.archCore.testing)
+    androidTestImplementation(AndroidX.test.rules)
+    androidTestImplementation("ch.tutteli.atrium", "atrium-fluent-en_GB", "_")
+    androidTestImplementation(Square.moshi.kotlinReflect)
+    androidTestImplementation("app.cash.turbine:turbine:0.2.0")
 }
