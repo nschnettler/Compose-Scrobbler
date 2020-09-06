6bbba99bd7edd801b70538ad8aadc25d13b9df2e
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-09-06 15:28:48 +0200

    Gradle: Remove com.android.application resolution strategy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,8 +19,6 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             when(requested.id.id) {
-                "com.android.application" ->
-                    useModule("com.android.tools.build:gradle:${requested.version}")
                 "dagger.hilt.android.plugin" ->
                     useModule("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha:${requested.version}")
             }
