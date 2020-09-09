bd123e328c1b3884b7c5da5047333a0facc23a4e
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2020-09-09 21:07:03 +0200

    ChartScreen.kt: Add Tab (Artist, Album, Track)

--- app/src/main/java/de/schnettler/scrobbler/screens/ChartScreen.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/ChartScreen.kt
@@ -2,14 +2,22 @@ package de.schnettler.scrobbler.screens
 
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumnForIndexed
 import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.onActive
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import de.schnettler.database.models.EntityType
 import de.schnettler.scrobbler.UIAction
 import de.schnettler.scrobbler.UIAction.ListingSelected
 import de.schnettler.scrobbler.UIError
@@ -18,6 +26,7 @@ import de.schnettler.scrobbler.components.IndexListIconBackground
 import de.schnettler.scrobbler.components.LoadingScreen
 import de.schnettler.scrobbler.components.SwipeRefreshProgressIndicator
 import de.schnettler.scrobbler.components.SwipeToRefreshLayout
+import de.schnettler.scrobbler.screens.charts.ChartTab
 import de.schnettler.scrobbler.util.abbreviate
 import de.schnettler.scrobbler.viewmodels.ChartsViewModel
 
@@ -30,6 +39,8 @@ fun ChartScreen(
 ) {
     onActive { model.startStream() }
     val chartState by model.state.collectAsState()
+
+    var currentChart by remember { mutableStateOf(ChartTab.Artist) }
 
     if (chartState.isError) {
         errorHandler(UIError.ShowErrorSnackbar(
@@ -46,13 +57,42 @@ fun ChartScreen(
             refreshIndicator = { SwipeRefreshProgressIndicator() }
         ) {
             chartState.currentData?.let { charts ->
-                LazyColumnForIndexed(items = charts, modifier) { index, (entry, artist) ->
-                    ChartListItem(artist.name, entry.count, index) { actionHandler(ListingSelected(artist)) }
-                    CustomDivider()
+                Column {
+                    TabRow(
+                        selectedTabIndex = currentChart.index,
+                        backgroundColor = MaterialTheme.colors.surface
+                    ) {
+                        val onSelect: (ChartTab) -> Unit = { currentChart = it }
+                        ChartsTab(
+                            tab = ChartTab.Artist,
+                            current = currentChart,
+                            onSelect = onSelect
+                        )
+                        ChartsTab(
+                            tab = ChartTab.Album,
+                            current = currentChart,
+                            onSelect = onSelect
+                        )
+                        ChartsTab(
+                            tab = ChartTab.Track,
+                            current = currentChart,
+                            onSelect = onSelect
+                        )
+                    }
+
+                    LazyColumnForIndexed(items = charts, modifier) { index, (entry, artist) ->
+                        ChartListItem(artist.name, entry.count, index) { actionHandler(ListingSelected(artist)) }
+                        CustomDivider()
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+private fun ChartsTab(tab: ChartTab, current: ChartTab, onSelect: (ChartTab) -> Unit) {
+    Tab(selected = tab == current, onClick = { onSelect(tab) }, text = { Text(text = tab.text) })
 }
 
 @Composable
---
+++ app/src/main/java/de/schnettler/scrobbler/screens/charts/ChartTab.kt
@@ -0,0 +1,7 @@
+package de.schnettler.scrobbler.screens.charts
+
+enum class ChartTab(val index: Int, val text: String) {
+    Artist(0, "Artist"),
+    Album(1, "Album"),
+    Track(2, "Track")
+}
