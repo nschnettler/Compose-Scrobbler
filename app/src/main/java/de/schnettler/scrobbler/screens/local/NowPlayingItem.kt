4f40443f97462bcdc52506413dbbe4db5843424e
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-11-08 15:36:32 +0100

    Remove unused import

--- app/src/main/java/de/schnettler/scrobbler/screens/local/NowPlayingItem.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/local/NowPlayingItem.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MusicNote
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import de.schnettler.scrobbler.components.PlainListIconBackground
 
