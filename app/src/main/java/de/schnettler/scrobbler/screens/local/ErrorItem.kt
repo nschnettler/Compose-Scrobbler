b7c2bd76b487efc4690041059cc970a0a470a606
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-12-19 14:47:08 +0100

    Cleanup

--- app/src/main/java/de/schnettler/scrobbler/screens/local/ErrorItem.kt
+++ app/src/main/java/de/schnettler/scrobbler/screens/local/ErrorItem.kt
@@ -1,6 +1,5 @@
 package de.schnettler.scrobbler.screens.local
 
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
