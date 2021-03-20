0fc457a449b206a4aa962f8d3a5cb68dbe2e363c
Niklas Schnettler <niklas.schnettler@gmail.com>
2021-03-20 13:42:16 +0100

    Rewrite MediaListenerService based on #40

---
+++ scrobble/src/main/java/de/schnettler/scrobble/LifecycleNotificationListenerService.kt
@@ -0,0 +1,51 @@
+package de.schnettler.scrobble
+
+import android.content.Intent
+import android.os.IBinder
+import android.service.notification.NotificationListenerService
+import androidx.annotation.CallSuper
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ServiceLifecycleDispatcher
+
+/**
+ * A NotificationListenerService that is also a [LifecycleOwner].
+ */
+open class LifecycleNotificationListenerService : NotificationListenerService(), LifecycleOwner {
+    private val dispatcher = ServiceLifecycleDispatcher(this)
+
+    @CallSuper
+    override fun onCreate() {
+        dispatcher.onServicePreSuperOnCreate()
+        super.onCreate()
+    }
+
+    @CallSuper
+    override fun onBind(intent: Intent?): IBinder? {
+        dispatcher.onServicePreSuperOnBind()
+        return null
+    }
+
+    @Suppress("deprecation")
+    @CallSuper
+    override fun onStart(intent: Intent?, startId: Int) {
+        dispatcher.onServicePreSuperOnStart()
+        super.onStart(intent, startId)
+    }
+
+    // this method is added only to annotate it with @CallSuper.
+    // In usual service super.onStartCommand is no-op, but in LifecycleService
+    // it results in mDispatcher.onServicePreSuperOnStart() call, because
+    // super.onStartCommand calls onStart().
+    @CallSuper
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @CallSuper
+    override fun onDestroy() {
+        dispatcher.onServicePreSuperOnDestroy()
+        super.onDestroy()
+    }
+
+    override fun getLifecycle() = dispatcher.lifecycle
+}
--- scrobble/src/main/java/de/schnettler/scrobble/ScrobbleState.kt
+++ scrobble/src/main/java/de/schnettler/scrobble/ScrobbleState.kt
@@ -1,19 +1,24 @@
 package de.schnettler.scrobble
 
+import android.media.MediaMetadata
+import android.media.session.MediaController
 import android.media.session.PlaybackState
 import de.schnettler.database.models.Scrobble
 import de.schnettler.scrobble.util.isPlaying
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class PlaybackController(
-    private val scrobbler: Scrobbler
-) {
+class ScrobbleState(
+    val controller: MediaController,
+    private val scrobbler: Scrobbler,
+    private val scope: CoroutineScope
+) : MediaController.Callback() {
+
     private var nowPlaying: Scrobble? = null
     private var lastPlaybackState: Int? = null
 
-    fun updateTrack(track: Scrobble) {
-        val wasPlaying = nowPlaying?.isPlaying() ?: true
-
+    private fun updateTrack(track: Scrobble) {
         when (track.isTheSameAs(nowPlaying)) {
             // Track is the same (title and artist match)
             true -> {
@@ -26,12 +31,12 @@ class PlaybackController(
                 // Save old Track
                 nowPlaying?.let {
                     it.pause()
-                    scrobbler.submitScrobble(it)
+                    scope.launch { scrobbler.submitScrobble(it) }
                 }
                 // Start new Track
                 Timber.d("[Controller] New Track $track")
                 nowPlaying = track
-                if (wasPlaying) {
+                if (controller.playbackState?.isPlaying() == true) {
                     track.play()
                     scrobbler.notifyNowPlaying(nowPlaying)
                 }
@@ -39,7 +44,7 @@ class PlaybackController(
         }
     }
 
-    fun updatePlayBackState(playbackState: PlaybackState) {
+    private fun updatePlayBackState(playbackState: PlaybackState) {
         val current = nowPlaying ?: return
         if (playbackState.state == lastPlaybackState) return
         lastPlaybackState = playbackState.state
@@ -49,7 +54,22 @@ class PlaybackController(
             Timber.d("[Controller] Resumed")
         } else {
             current.pause()
+            nowPlaying?.let {
+                scope.launch {
+                    if (scrobbler.submitScrobble(it)) {
+                        nowPlaying?.amountPlayed = 0
+                    }
+                }
+            }
             Timber.d("[Controller] Paused")
         }
+    }
+
+    override fun onMetadataChanged(metadata: MediaMetadata?) {
+        metadata?.let { updateTrack(Scrobble.fromMetadata(metadata, controller.packageName)) }
+    }
+
+    override fun onPlaybackStateChanged(state: PlaybackState?) {
+        state?.let { updatePlayBackState(state) }
     }
 }
