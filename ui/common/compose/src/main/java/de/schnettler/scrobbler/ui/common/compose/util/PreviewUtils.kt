48fdef1108290e0ff672d2d805770c5c359c3a96
Niklas Schnettler <niklas.schnettler@gmail.com>
2021-03-28 15:27:05 +0200

    Add ProfileScreen preview

--- ui/common/compose/src/main/java/de/schnettler/scrobbler/ui/common/compose/util/PreviewUtils.kt
+++ ui/common/compose/src/main/java/de/schnettler/scrobbler/ui/common/compose/util/PreviewUtils.kt
@@ -2,10 +2,18 @@ package de.schnettler.scrobbler.ui.common.compose.util
 
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import de.schnettler.database.models.EntityType
+import de.schnettler.database.models.LastFmEntity
+import de.schnettler.database.models.ListType
+import de.schnettler.database.models.TopListAlbum
+import de.schnettler.database.models.TopListArtist
+import de.schnettler.database.models.TopListEntry
+import de.schnettler.database.models.TopListTrack
+import de.schnettler.database.models.User
 import de.schnettler.scrobbler.ui.common.compose.theme.AppTheme
 
 @Composable
-internal fun ThemedPreview(
+fun ThemedPreview(
     darkTheme: Boolean = false,
     content: @Composable () -> Unit
 ) {
@@ -13,5 +21,59 @@ internal fun ThemedPreview(
         Surface {
             content()
         }
+    }
+}
+
+object PreviewUtils {
+    @Composable
+    fun generateFakeUser() = User(
+        "Nickname",
+        10L,
+        "",
+        10,
+        "Realname",
+        10,
+        "Germany",
+        "",
+        10
+    )
+
+    fun generateFakeArtistCharts(number: Int) = MutableList(number) { index ->
+        TopListArtist(
+            TopListEntry(
+                "",
+                EntityType.ARTIST,
+                ListType.CHART,
+                index,
+                10
+            ),
+            LastFmEntity.Artist("Artist $index")
+        )
+    }
+
+    fun generateFakeAlbumCharts(number: Int) = MutableList(number) { index ->
+        TopListAlbum(
+            TopListEntry(
+                "",
+                EntityType.ALBUM,
+                ListType.CHART,
+                index,
+                10
+            ),
+            LastFmEntity.Album("Album $index", artist = "Artist $index")
+        )
+    }
+
+    fun generateFakeTrackCharts(number: Int) = MutableList(number) { index ->
+        TopListTrack(
+            TopListEntry(
+                "",
+                EntityType.TRACK,
+                ListType.CHART,
+                index,
+                10
+            ),
+            LastFmEntity.Track("Track $index", artist = "Artist $index")
+        )
     }
 }
--- ui/profile/src/main/java/de/schnettler/scrobbler/ui/profile/ProfileScreen.kt
+++ ui/profile/src/main/java/de/schnettler/scrobbler/ui/profile/ProfileScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.CoilImage
@@ -53,6 +54,8 @@ import de.schnettler.scrobbler.ui.common.compose.model.MediaCardSize
 import de.schnettler.scrobbler.ui.common.compose.navigation.UIAction
 import de.schnettler.scrobbler.ui.common.compose.navigation.UIError
 import de.schnettler.scrobbler.ui.common.compose.theme.AppColor
+import de.schnettler.scrobbler.ui.common.compose.util.PreviewUtils
+import de.schnettler.scrobbler.ui.common.compose.util.ThemedPreview
 import de.schnettler.scrobbler.ui.common.compose.widget.Carousel
 import de.schnettler.scrobbler.ui.common.compose.widget.FullScreenLoading
 import de.schnettler.scrobbler.ui.common.compose.widget.LoadingContent
@@ -302,4 +305,19 @@ private fun PeriodSelectDialog(
             }
         }
     )
+}
+
+@Preview
+@Composable
+private fun ProfileContentPreview() = ThemedPreview {
+    ProfileContent(
+        modifier = Modifier,
+        user = PreviewUtils.generateFakeUser(),
+        artists = PreviewUtils.generateFakeArtistCharts(3),
+        albums = PreviewUtils.generateFakeAlbumCharts(3),
+        tracks = PreviewUtils.generateFakeTrackCharts(10),
+        timePeriod = UITimePeriod.OVERALL,
+        cardSize = MediaCardSize.SMALL.size,
+        onFabClicked = { /*TODO*/ }) {
+    }
 }
