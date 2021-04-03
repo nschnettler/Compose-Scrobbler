429d4b410f1c116bdc1d72f1f4e9f9b81c8b6501
Niklas Schnettler <niklas.schnettler@gmail.com>
2021-04-03 17:20:15 +0200

    Refactor ProfileScreen to use single state
    - Introduce StoreViewModel & ReduxViewModel
    - Introduce ProfileViewState

---
+++ ui/common/compose/src/main/java/de/schnettler/scrobbler/ui/common/compose/viewmodel/ReduxViewModel.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.schnettler.scrobbler.ui.common.compose.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+@OptIn(InternalCoroutinesApi::class)
+abstract class ReduxViewModel<S> constructor(initialState: S) : ViewModel() {
+    private val _state = MutableStateFlow(initialState)
+    private val stateMutex = Mutex()
+
+    val state: StateFlow<S>
+        get() = _state.asStateFlow()
+
+    protected suspend fun setState(reducer: S.() -> S) {
+        stateMutex.withLock {
+            _state.value = reducer(_state.value)
+        }
+    }
+}
--- ui/profile/src/main/java/de/schnettler/scrobbler/ui/profile/ProfileViewModelImpl.kt
+++ ui/profile/src/main/java/de/schnettler/scrobbler/ui/profile/ProfileViewModelImpl.kt
@@ -2,19 +2,11 @@ package de.schnettler.scrobbler.ui.profile
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import de.schnettler.database.models.TopListAlbum
-import de.schnettler.database.models.TopListArtist
-import de.schnettler.database.models.TopListTrack
-import de.schnettler.database.models.User
 import de.schnettler.datastore.manager.DataStoreManager
 import de.schnettler.repo.TopListRepository
 import de.schnettler.repo.UserRepository
 import de.schnettler.repo.preferences.PreferenceEntry
-import de.schnettler.scrobbler.ui.common.compose.RefreshableUiState
-import de.schnettler.scrobbler.ui.common.compose.freshFrom
 import de.schnettler.scrobbler.ui.common.compose.model.MediaCardSize
-import de.schnettler.scrobbler.ui.common.compose.refreshStateFlowFromStore
-import de.schnettler.scrobbler.ui.common.compose.streamFrom
 import de.schnettler.scrobbler.ui.common.compose.updateValue
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -26,24 +18,10 @@ import javax.inject.Inject
 class ProfileViewModelImpl @Inject constructor(
     private val topListRepo: TopListRepository,
     private val userRepo: UserRepository,
-    private val dataStoreManager: DataStoreManager
+    dataStoreManager: DataStoreManager
 ) : ProfileViewModel() {
     override val timePeriod = MutableStateFlow(UITimePeriod.OVERALL)
-
-    override val albumState: MutableStateFlow<RefreshableUiState<List<TopListAlbum>>> =
-        MutableStateFlow(RefreshableUiState.Success(data = null, loading = true))
-
-    override val artistState: MutableStateFlow<RefreshableUiState<List<TopListArtist>>> =
-        MutableStateFlow(RefreshableUiState.Success(data = null, loading = true))
-
-    override val trackState: MutableStateFlow<RefreshableUiState<List<TopListTrack>>> =
-        MutableStateFlow(RefreshableUiState.Success(data = null, loading = true))
-
-    override val userState: MutableStateFlow<RefreshableUiState<User>> =
-        MutableStateFlow(RefreshableUiState.Success(data = null, loading = true))
-
     override val showFilterDialog: MutableStateFlow<Boolean> = MutableStateFlow(false)
-
     override val mediaCardSize = dataStoreManager.getPreferenceFlow(PreferenceEntry.MediaCardSize).map {
         try {
             MediaCardSize.valueOf(it)
@@ -52,29 +30,35 @@ class ProfileViewModelImpl @Inject constructor(
         }.size
     }
 
+    override fun updatePeriod(period: UITimePeriod) = timePeriod.updateValue(period)
+    override fun showDialog(show: Boolean) = showFilterDialog.updateValue(show)
+
     init {
+        userRepo.userStore.streamIntoState("") { state, user ->
+            state?.copy(user = user) ?: ProfileViewState(user = user)
+        }
+
         viewModelScope.launch {
-            launch { userState.streamFrom(userRepo.userStore, "") }
-            launch { refreshStateFlowFromStore(null, userRepo.lovedTracksStore, "") }
-            timePeriod.collectLatest { uiTime ->
-                launch { artistState.streamFrom(topListRepo.topArtistStore, uiTime.period) }
-                launch { albumState.streamFrom(topListRepo.topAlbumStore, uiTime.period) }
-                launch { trackState.streamFrom(topListRepo.topTracksStore, uiTime.period) }
+            timePeriod.collectLatest {
+                topListRepo.topArtistStore.streamIntoState(it.period) { state, toplist ->
+                    state?.copy(topArtists = toplist) ?: ProfileViewState(topArtists = toplist)
+                }
+                topListRepo.topAlbumStore.streamIntoState(it.period) { state, toplist ->
+                    state?.copy(topAlbums = toplist) ?: ProfileViewState(topAlbums = toplist)
+                }
+                topListRepo.topTracksStore.streamIntoState(it.period) { state, toplist ->
+                    state?.copy(topTracks = toplist) ?: ProfileViewState(topTracks = toplist)
+                }
             }
         }
     }
 
     override fun refresh() {
-        viewModelScope.apply {
-            launch { artistState.freshFrom(topListRepo.topArtistStore, timePeriod.value.period) }
-            launch { albumState.freshFrom(topListRepo.topAlbumStore, timePeriod.value.period) }
-            launch { trackState.freshFrom(topListRepo.topTracksStore, timePeriod.value.period) }
-            launch { userState.freshFrom(userRepo.userStore, "") }
-            launch { refreshStateFlowFromStore(null, userRepo.lovedTracksStore, "") }
+        userRepo.userStore.refreshIntoState("")
+        timePeriod.value.period.also {
+            topListRepo.topAlbumStore.refreshIntoState(it)
+            topListRepo.topTracksStore.refreshIntoState(it)
+            topListRepo.topArtistStore.refreshIntoState(it)
         }
     }
-
-    override fun updatePeriod(period: UITimePeriod) = timePeriod.updateValue(period)
-
-    override fun showDialog(show: Boolean) = showFilterDialog.updateValue(show)
 }
