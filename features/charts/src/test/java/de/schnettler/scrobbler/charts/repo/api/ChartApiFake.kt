f60c97d13b0d4d62104f613f120600b914069043
Niklas Schnettler <niklas.schnettler@gmail.com>
2021-06-12 11:11:30 +0200

    Fix test compilation

--- features/charts/src/test/java/de/schnettler/scrobbler/charts/repo/api/ChartApiFake.kt
+++ features/charts/src/test/java/de/schnettler/scrobbler/charts/repo/api/ChartApiFake.kt
@@ -4,14 +4,21 @@ import de.schnettler.scrobbler.charts.api.ChartApi
 import de.schnettler.scrobbler.charts.model.ChartArtistResponse
 import de.schnettler.scrobbler.charts.model.ChartTrackResponse
 import de.schnettler.scrobbler.charts.repo.tools.ApiTestUtils
+import retrofit2.http.Query
 
 class ChartApiFake : ChartApi, ApiTestUtils() {
 
-    override suspend fun getTopArtists(): List<ChartArtistResponse> {
+    override suspend fun getTopArtists(
+        @Query(value = "page") page: Int,
+        @Query(value = "limit") limit: Int
+    ): List<ChartArtistResponse> {
         return moshi.parseJsonList(getJsonStringFromFile("topArtists")) ?: emptyList()
     }
 
-    override suspend fun getTopTracks(): List<ChartTrackResponse> {
+    override suspend fun getTopTracks(
+        @Query(value = "page") page: Int,
+        @Query(value = "limit") limit: Int
+    ): List<ChartTrackResponse> {
         TODO("Not yet implemented")
     }
 }
